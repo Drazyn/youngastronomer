f2ca52769226f9b97192c8882731baa7eddf27c9
Drazyn <gabriellucena360@outlook.com>
2020-10-05 12:00:31 -0300

    Draft of the styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
+var pageContent = () => {
+    return (
+      <div>
+        <h1>Study guide</h1>
+        <h2>OBA - Level 4</h2>
+        Practice your skills with past papers.
+        <h2>Seletiva Online</h2>
+        Practice your skills with past papers.
+        <h2>Barra do Pirai</h2>
+        Practice your skills with past papers.
+        <h2>Treinamento em Vinhedo</h2>
+        Practice your skills with past papers.
+        <h2>Internacionais</h2>
+        Practice your skills with past papers.
+      </div>
+    );
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Hello Young Astronomers! This personal blog is my own way to share my astronomy experience for everyone. Are you ready?
-      </header>
+      <div className="insideBox">
+          <header>
+            Young Astronomer Header
+          </header>
+          <div>
+              {pageContent()}
+          </div>
+      </div>
+
+      <footer>
+        Nao sabemos nada sobre licenca, vamos pesquisar!
+      </footer>
+
     </div>
   );
 }
